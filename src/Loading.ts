e4adba279e32701ba600bed6309032c6639450f9
eightfeet <bajiao000000@163.com>
2021-04-28 11:35:50 +0800

    update

--- src/Loading.ts
+++ src/Loading.ts
@@ -3,75 +3,70 @@ import { createDom, removeDom } from './htmlFactory';
 import template from './temolate';
 
 export interface Parameters {
-    /**
-     * loadingId 不传自动生成 loading + 时间戳 + 100以内的随机数
-     * @type {string}
-     * @memberof Parameters
-     */
-    id?: string;
-    /**
-     * loading 层级
-     * @type {number}
-     * @memberof Parameters
-     */
-    zIndex?: string;
-    /**
-     * loading样式
-     * @type {{
-     *         wrap?: CSS.Properties;
-     *         main?: CSS.Properties;
-     *     }}
-     * @memberof Parameters
-     */
-    style?: {
-        overlay?: CSS.Properties;
-        content?: CSS.Properties;
-        vertices?: {
-<<<<<<< HEAD
-            elements?: string[];
-            size?: string;
-=======
-            elements?: string[],
-            size?: string,
->>>>>>> 8b97a2fc94ff3573edd53914dcc53356382ef6dd
-        } & CSS.Properties;
-    };
-    
-    /**
-     * 基准文字大小
-     * @type {number}
-     * @memberof Parameters
-     */
-    emBase?: number;
-    /**
-     * 周期时间
-     * @type {string}
-     * @memberof Parameters
-     */
-    cycleTime?: number;
-    /**
-     * 父级Id
-     * @type {string}
-     * @memberof Parameters
-     */
-    parentId?: string;
-    /**
-     * 由几个vertices组成默认12个
-     * @type {number}
-     * @memberof Parameters
-     */
-    length?: number;
+	/**
+	 * loadingId 不传自动生成 loading + 时间戳 + 100以内的随机数
+	 * @type {string}
+	 * @memberof Parameters
+	 */
+	id?: string;
+	/**
+	 * loading 层级
+	 * @type {number}
+	 * @memberof Parameters
+	 */
+	zIndex?: string;
+	/**
+	 * loading样式
+	 * @type {{
+	 *         wrap?: CSS.Properties;
+	 *         main?: CSS.Properties;
+	 *     }}
+	 * @memberof Parameters
+	 */
+	style?: {
+		overlay?: CSS.Properties;
+		content?: CSS.Properties;
+		vertices?: {
+			elements?: string[];
+			size?: string;
+		} & CSS.Properties;
+	};
+	
+	/**
+	 * 基准文字大小
+	 * @type {number}
+	 * @memberof Parameters
+	 */
+	emBase?: number;
+	/**
+	 * 周期时间
+	 * @type {string}
+	 * @memberof Parameters
+	 */
+	cycleTime?: number;
+	/**
+	 * 父级Id
+	 * @type {string}
+	 * @memberof Parameters
+	 */
+	parentId?: string;
+	/**
+	 * 由几个vertices组成默认12个
+	 * @type {number}
+	 * @memberof Parameters
+	 */
+	length?: number;
 }
 
 class Loading {
-    emBase: number;
-    id: string;
-    counter: number;
-    style: any;
-    length: number;
-    cycleTime: number;
-    parentId: string;
-    zIndex: number;
+	emBase: number;
+	id: string;
+	counter: number;
+	style: any;
+	length: number;
+	cycleTime: number;
+	parentId: string;
+	zIndex: number;
 	constructor(parameters: Parameters) {
 		const stamp = (new Date()).getTime();
 		const {style, id, length, cycleTime, parentId, emBase, zIndex} = parameters || {};
